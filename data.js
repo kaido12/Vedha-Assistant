6d7c0f72a74a9f694ab0a79ded0340ebc50d8b91
Avnish <37014011+kaido12@users.noreply.github.com>
2022-07-03 09:31:24 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,29 @@
+let lists = [
+    {
+      question: "what is your name",
+      answer: "my name is Vedha.",
+    },
+    {
+      question: "what's your name",
+      answer: "my name is Vedha.",
+    },
+    {
+      question: "what do you do",
+      answer: "I help developers write better code.",
+    },
+    {
+      question: "introduce yourself",
+      answer:
+        "my name is Vedha, I am a Java Script program created by Sonu to make things easier for developers and all humanity.",
+    },
+    {
+      question: "What is Artificial Intelligence",
+      answer: 
+      "artificial intelligence (AI), the ability of a digital computer or computer-controlled robot to perform tasks commonly associated with intelligent beings. The term is frequently applied to the project of developing systems endowed with the intellectual processes characteristic of humans, such as the ability to reason, discover meaning, generalize, or learn from past experience.",
+    },
+    {
+      question: "Who is the president of India",
+      answer: "The current president in 2022 is Ram Nath Kovind.",
+    },
+  ];
+  
